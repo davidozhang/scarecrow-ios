6656b2dc083a5786dcfc4c554f46104f98fc228c
David Zhang <davzee@hotmail.com>
2015-11-21 14:54:05 -0500

    Add collection view of dummy devices connected to Edison

---
+++ Scarecrow/Scarecrow/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  Scarecrow
+//
+//  Created by David on 11/21/15.
+//  Copyright © 2015 David Zhang. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var toggle: UISwitch!
+    
+    
+}
--- Scarecrow/Scarecrow/ViewController.swift
+++ Scarecrow/Scarecrow/ViewController.swift
@@ -12,18 +12,34 @@ import LocalAuthentication
 let authenticatedKey = "authenticated"
 let unauthenticatedKey = "unauthenticated"
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    var devices = ["device 1", "device 2", "device 3", "device 4", "device 5", "device 6", "device 7", "device 8", "device 9", "device 10"]
+    
     @IBOutlet var status: UIView!
     @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var devicesCollection: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "setAuthenticated", name: authenticatedKey, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "setUnauthenticated", name: unauthenticatedKey, object: nil)
+        devicesCollection.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.7)
+        hideDevices()
         setUnauthenticated()
         authenticateWithTouchID()
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return devices.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: CollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.name.text = devices[indexPath.row]
+        return cell
+    }
+    
     func setUnauthenticated() {
         status.backgroundColor = UIColor(red: 0.9, green: 0, blue: 0, alpha: 0.6)
         statusLabel.text = "Unauthenticated"
@@ -32,6 +48,14 @@ class ViewController: UIViewController {
     func setAuthenticated() {
         status.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 0.6)
         statusLabel.text = "Authenticated"
+    }
+    
+    func showDevices() {
+        devicesCollection.hidden = false
+    }
+    
+    func hideDevices() {
+        devicesCollection.hidden = true
     }
     
     func showAlert(message: String) {
@@ -52,11 +76,13 @@ class ViewController: UIViewController {
                     self.showAlert("Scarecrow successfully authenticated")
                     dispatch_async(dispatch_get_main_queue(), {
                         NSNotificationCenter.defaultCenter().postNotificationName("authenticated", object: nil)
+                        self.showDevices()
                     })
                 } else {
                     self.showAlert("Scarecrow authentication failed")
                     dispatch_async(dispatch_get_main_queue(), {
                         NSNotificationCenter.defaultCenter().postNotificationName("unauthenticated", object: nil)
+                        self.hideDevices()
                     })
                 }
             })
